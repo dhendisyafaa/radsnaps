2ab3b4f4dcbe67cd89b7e5124f5917d55c833171
Dhendi Syafa Athallah Putra <dhendisyafa@gmail.com>
2024-09-01 19:36:21 +0700

    feat: add function for post content

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,7 +1,24 @@
-"use client";
-import Image from "next/image";
-import { logout } from "../(auth)/actions";
+import PostEditor from "@/components/posts/editor/PostEditor";
+import PostComponent from "@/components/posts/PostComponent";
+import prisma from "@/lib/prisma";
+import { postDataInclude } from "@/lib/types";
 
-export default function Home() {
-  return <div className="">hello</div>;
+export default async function Home() {
+  const posts = await prisma.post.findMany({
+    include: postDataInclude,
+    orderBy: { createdAt: "desc" },
+  });
+
+  return (
+    <main className="w-full min-w-0">
+      <div className="w-full min-w-0 space-y-5">
+        <div className="hidden sm:block">
+          <PostEditor />
+        </div>
+        {posts.map((post) => (
+          <PostComponent key={post.id} post={post} />
+        ))}
+      </div>
+    </main>
+  );
 }
