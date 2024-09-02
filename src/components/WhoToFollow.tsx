d866958d5f2dfe7d0b1d745ec35176a4338497fb
Dhendi Syafa Athallah Putra <dhendisyafa@gmail.com>
2024-09-02 11:19:28 +0700

    feat: split component trending sidebar

---
+++ src/components/WhoToFollow.tsx
@@ -0,0 +1,47 @@
+import { validateRequest } from "@/auth";
+import prisma from "@/lib/prisma";
+import Link from "next/link";
+import UserAvatar from "./UserAvatar";
+import { Button } from "./ui/button";
+import { userDataSelect } from "@/lib/types";
+
+export default async function WhoToFollow() {
+  const { user } = await validateRequest();
+
+  if (!user) return null;
+
+  const usersToFollow = await prisma.user.findMany({
+    where: {
+      NOT: {
+        id: user.id,
+      },
+    },
+    select: userDataSelect,
+    take: 5,
+  });
+
+  return (
+    <div className="space-y-5 rounded-2xl bg-card p-5 shadow-sm">
+      <div className="text-xl font-bold">Who to follow</div>
+      {usersToFollow.map((user) => (
+        <div key={user.id} className="flex items-center justify-between gap-3">
+          <Link
+            href={`/users/${user.username}`}
+            className="flex items-center gap-3"
+          >
+            <UserAvatar avatarUrl={user.avatarUrl} className="flex-none" />
+            <div>
+              <p className="line-clamp-1 break-all font-semibold hover:underline">
+                {user.displayName}
+              </p>
+              <p className="line-clamp-1 break-all text-muted-foreground">
+                @{user.username}
+              </p>
+            </div>
+          </Link>
+          <Button>Follow</Button>
+        </div>
+      ))}
+    </div>
+  );
+}
